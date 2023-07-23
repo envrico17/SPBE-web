4187c00c4befcfc2217b2c06a4739e5510d022d3
Alvian Rahmadani <47630429+Archiruz@users.noreply.github.com>
2023-07-23 20:49:00 +0700

    chore: add jquery support (#50)

--- SPBE-web/resources/views/components/layout.blade.php
+++ SPBE-web/resources/views/components/layout.blade.php
@@ -57,6 +57,8 @@
     }
 
 </script>
+{{-- jQuery --}}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 {{-- Select2 plugin --}}
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
